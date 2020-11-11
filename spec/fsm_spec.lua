b05162584b7aadceb76f999a21018ed9a7280753
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2020-11-11 08:09:36 +0000

    tcp_yeah: don't set ssthresh below 2

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -3,4 +3,5 @@ require("busted")
 local machine = require("statemachine")
 local _ = require("luassert.match")._
 
-describe("Lua state machine framework
+describe("Lua state machine framework", function()
+  descr
