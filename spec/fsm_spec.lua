81709ac734253100c09d962b536b54adf835d922
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2020-12-16 11:21:42 +0000

    Removed.

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -37,4 +37,6 @@ describe("Lua state machine framework", function()
       assert.is_false(fsm:can('panic'))
       assert.is_false(fsm:can('calm'))
       assert.is_false(fsm:can('clear'))
-    
+    end)
+
+    it("shoul
