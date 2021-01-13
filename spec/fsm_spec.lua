10efedf594a78212d42b3b11f0988d16dd669c0c
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-01-13 19:36:11 +0000

    change headers

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -59,4 +59,5 @@ describe("Lua state machine framework", function()
         callbacks = {
           onbeforewarn = stub.new(),
           onleavegreen = stub.new(),
-          onenteryellow 
+          onenteryellow = stub.new(),
+          onaft
