da27c3c4bbbee4804bfa432cc654cafcf462c2b9
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-01-15 07:22:29 +0000

    update appveyor

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -62,4 +62,4 @@ describe("Lua state machine framework", function()
           onenteryellow = stub.new(),
           onafterwarn = stub.new(),
           onstatechange = stub.new(),
-   
+          onyellow = stub.new(),
