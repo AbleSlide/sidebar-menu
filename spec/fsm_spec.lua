c98738bc198160af0fc18e8a635213a4fa7b85e9
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-01-19 12:55:09 +0000

    Create pythonapp.yml

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -72,4 +72,5 @@ describe("Lua state machine framework", function()
       assert.spy(fsm.onbeforewarn).was_called_with(_, 'warn', 'green', 'yellow')
       assert.spy(fsm.onleavegreen).was_called_with(_, 'warn', 'green', 'yellow')
 
-      assert.spy(fsm.onenteryellow).was_called_with(_, 'warn', 'green', 'yellow
+      assert.spy(fsm.onenteryellow).was_called_with(_, 'warn', 'green', 'yellow')
+      assert.s
