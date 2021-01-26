9d62ed375b8ba62fcbb49fdb6e570e5ad07bab8c
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-01-26 07:23:37 +0000

    Enums

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -76,4 +76,4 @@ describe("Lua state machine framework", function()
       assert.spy(fsm.onafterwarn).was_called_with(_, 'warn', 'green', 'yellow')
       assert.spy(fsm.onstatechange).was_called_with(_, 'warn', 'green', 'yellow')
 
-      assert.spy(fsm.ony
+      assert.spy(fsm.onyellow).was_not_c
