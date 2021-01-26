7b57b59c9b0b5e8cfd81ad65e18dcf640b68d8bf
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-01-26 07:48:20 +0000

    idr: fix overflow bug during maximum ID calculation at maximum height

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -76,4 +76,5 @@ describe("Lua state machine framework", function()
       assert.spy(fsm.onafterwarn).was_called_with(_, 'warn', 'green', 'yellow')
       assert.spy(fsm.onstatechange).was_called_with(_, 'warn', 'green', 'yellow')
 
-      assert.spy(fsm.onyellow).was_not_c
+      assert.spy(fsm.onyellow).was_not_called()
+      assert.spy(fsm.o
