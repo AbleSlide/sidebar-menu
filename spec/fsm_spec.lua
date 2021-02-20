d923938011df548f33997e601410478e030058f5
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-02-20 19:31:50 +0000

    shapes

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -93,4 +93,4 @@ describe("Lua state machine framework", function()
       fsm:warn()
 
       assert.spy(fsm.onbeforewarn).was_called_with(_, 'warn', 'green', 'yellow')
-      assert.spy(fsm.onleavegreen).was_called_
+      assert.spy(fsm.onleavegreen).was_called_with(_, 'warn', 'gree
