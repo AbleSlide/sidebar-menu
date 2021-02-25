132c776c02de8c99f7cd59a30db36898a6930303
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-02-25 16:15:23 +0000

    Error message on form.

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -95,4 +95,4 @@ describe("Lua state machine framework", function()
       assert.spy(fsm.onbeforewarn).was_called_with(_, 'warn', 'green', 'yellow')
       assert.spy(fsm.onleavegreen).was_called_with(_, 'warn', 'green', 'yellow')
 
-  
+      assert.spy(
