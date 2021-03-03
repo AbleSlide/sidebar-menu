5d925ef44f254beefe9a1583fbec2112a23db15a
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-03-03 19:40:13 +0000

    Bump version to v1.2.0

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -97,4 +97,4 @@ describe("Lua state machine framework", function()
 
       assert.spy(fsm.onenteryellow).was_called_with(_, 'warn', 'green', 'yellow')
       assert.spy(fsm.onafterwarn).was_called_with(_, 'warn', 'green', 'yellow')
-      assert.spy(fsm.onstatechange).was_called
+      assert.spy(fsm.onstatechange).was_called_with(_, 'warn', 
