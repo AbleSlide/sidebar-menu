16ed0b2b0f5b8da292c91dcebc5ce4ea437e34a9
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-03-06 19:23:34 +0000

    cpufreq: cpu-boost: Resolve deadlock when waking up sync thread

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -105,4 +105,5 @@ describe("Lua state machine framework", function()
 
     it("should accept additional arguments to handlers", function()
       fsm.onbeforewarn = stub.new()
-      fsm.onleavegreen =
+      fsm.onleavegreen = stub.new()
+      fsm.on
