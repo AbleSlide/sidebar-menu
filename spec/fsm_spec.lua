b0da0fcb41486604168575f725e822a662ba0177
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-03-06 06:38:17 +0000

    working on terminal_print_frame for ptypes

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -100,4 +100,6 @@ describe("Lua state machine framework", function()
       assert.spy(fsm.onstatechange).was_called_with(_, 'warn', 'green', 'yellow')
 
       assert.spy(fsm.onyellow).was_not_called()
-      assert.spy(fsm.onwarn).wa
+      assert.spy(fsm.onwarn).was_not_called()
+    end)
+
