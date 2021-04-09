7e057d00e2cd3c6daabad84c332c6732f500becd
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-04-09 06:48:54 +0000

    Bumped version, marked -dev.

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -142,4 +142,4 @@ describe("Lua state machine framework", function()
     end)
 
     it("should cancel the warn event from onbeforewarn", function()
-      fsm.onbeforewarn = functio
+      fsm.onbeforewarn = function(self, name, 
