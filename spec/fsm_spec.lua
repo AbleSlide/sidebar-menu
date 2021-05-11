0aab5f223840c0cb874c08622fa6b9778bfd0470
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-05-11 09:00:35 +0000

    index updates

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -173,4 +173,5 @@ describe("Lua state machine framework", function()
       assert.are_equal(fsm.asyncState, 'warnWaitingOnEnter')
 
       result = fsm:transition(fsm.currentTransitioningEvent)
-   
+      assert.is_true(result)
+  
