e930454b0fecc8c5136c7555c6ed560cf472d263
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-05-17 18:14:45 +0000

    Minor changes. Commit message skipped.

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -175,4 +175,5 @@ describe("Lua state machine framework", function()
       result = fsm:transition(fsm.currentTransitioningEvent)
       assert.is_true(result)
       assert.are_equal(fsm.current, 'yellow')
-      assert.is_nil(fsm.currentTransitio
+      assert.is_nil(fsm.currentTransitioningEvent)
+      assert.a
