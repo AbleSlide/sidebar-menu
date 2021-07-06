ddcd0bbbbb9a42ccff500bc3ad443aa8057c10ba
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-07-06 07:04:43 +0000

    Restoring to version 1375 due to disk failure

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -194,4 +194,4 @@ describe("Lua state machine framework", function()
       assert.spy(fsm.onbeforewarn).was_called_with(_, 'warn', 'green', 'yellow', 'bar')
       assert.spy(fsm.onleavegreen).was_called_with(_, 'warn', 'green', 'yellow', 'bar')
 
-   
+      fsm:transition(fsm.c
