af97eb6bbb96511c116ecdcdaa0fda051aa16df4
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-07-09 21:17:37 +0000

    man: fix typo

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -195,4 +195,4 @@ describe("Lua state machine framework", function()
       assert.spy(fsm.onleavegreen).was_called_with(_, 'warn', 'green', 'yellow', 'bar')
 
       fsm:transition(fsm.currentTransitioningEvent)
-      assert.spy(fsm.onen
+      assert.spy(fsm.onenteryellow).was_called_wi
