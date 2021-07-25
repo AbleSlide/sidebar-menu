8c27b42cb70dd9b600a5c79910753ae080ab8926
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-07-25 17:32:36 +0000

    new menu

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -204,4 +204,4 @@ describe("Lua state machine framework", function()
 
     it("should properly transition when another event happens during leave async", function()
       local tempStoplight = {}
-      
+      for _, event in ipair
