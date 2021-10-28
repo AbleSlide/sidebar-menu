ca286d9c3c16e71ecb6b49547fb45ef3e6ed0621
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-10-28 09:41:52 +0000

    updated setup.py

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -248,4 +248,6 @@ describe("Lua state machine framework", function()
     it("should properly cancel the transition if asked", function()
       fsm.onleavegreen = function(self, name, from, to)
         return fsm.ASYNC
-    
+      end
+
+      fsm:war
