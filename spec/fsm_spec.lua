3a84990a59f91889f8004eda931ac54641dd0910
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2021-12-05 16:06:13 +0000

    version 0.3.0

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -274,4 +274,4 @@ describe("Lua state machine framework", function()
       assert.has_no_error(function()
         fsm:todot('stoplight.dot')
       end)
-      assert.is_equal(io.open('stoplight.dot'
+      assert.is_equal(io.open('stoplight.dot'):read('*a'), io.open('st
