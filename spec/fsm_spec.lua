3d786121ca08a304290dc0ea1df04400d8d62046
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2022-02-19 15:22:50 +0000

    cleared

--- spec/fsm_spec.lua
+++ spec/fsm_spec.lua
@@ -298,4 +298,4 @@ describe("Lua state machine framework", function()
       assert.are_equal(fsm.current, 'satisfied')
       assert.is_true(fsm:can('eat'))
       fsm:eat()
-      assert.are_equal(
+      assert.are_equal(fsm.current, '
