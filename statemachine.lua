9fc3cfb4232938f676f4d61529318d6ae9c894ea
AbleSlide <120669694+AbleSlide@users.noreply.github.com>
2022-06-06 20:40:17 +0000

    Fix cast

--- statemachine.lua
+++ statemachine.lua
@@ -15,4 +15,5 @@ local function create_transition(name)
 
   local function transition(self, ...)
     if self.asyncState == NONE then
-      can, to = self:can(na
+      can, to = self:can(name)
+      from = self.curre
